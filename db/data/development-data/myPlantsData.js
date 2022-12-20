7e1b4489fae859a5cbc83d67b3ce0fd9563b4f58
malik016 <63663632+malik9153@users.noreply.github.com>
2022-12-20 16:53:51 +0000

    added myPLantsTable

---
+++ db/data/development-data/myPlantsData.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    user_id: 1,
+    plant_id: 25,
+    last_watered: 5,
+  },
+  {
+    user_id: 2,
+    plant_id: 50,
+    last_watered: 10,
+  },
+];
--- db/data/test-data/index.js
+++ db/data/test-data/index.js
@@ -1,2 +1,3 @@
 exports.plantsData = require("./plants.js");
 exports.userData = require("./users")
+exports.myPlantsData = require("./myPlantsData");
